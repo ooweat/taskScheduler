2fd58204db5f59a13a983280328dcd1f61ceb197
chris_kim <twkim326@gmail.com>
2023-03-23 10:03:46 +0900

    Controller 리뉴얼.
    Oracle -> Maria 전환
    스케줄러 옵션명칭 변경으로 @Sechduled 값 수정

--- src/main/java/kr/co/ooweat/taskScheduler/controller/BatchController.java
+++ src/main/java/kr/co/ooweat/taskScheduler/controller/BatchController.java
@@ -1,7 +1,10 @@
 package kr.co.ooweat.taskScheduler.controller;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import javax.mail.MessagingException;
 import kr.co.ooweat.taskScheduler.common.Util;
-import kr.co.ooweat.taskScheduler.mappers.oracle.manager.ManagerMapper;
+import kr.co.ooweat.taskScheduler.mappers.maria.manager.ManagerMapper;
 import kr.co.ooweat.taskScheduler.service.BatchService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +14,13 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.mail.MessagingException;
-import java.io.UnsupportedEncodingException;
+/**
+ * @author chris
+ * @version 1.2.1
+ * @class BatchController
+ * @Created 2022-06-21
+ * @Desc : 기존 crontab을 대체할 Spring Batch
+ **/
 
 @Slf4j
 @CrossOrigin("*")
@@ -25,75 +33,74 @@ public class BatchController {
 
 	@Autowired
 	private ManagerMapper managerMapper;
-
 	@Value("${charge.use.flag}")
 	private String chargeFlag;
 
-	//HINT: workDayCheck() - Oracle DB 기준으로 작업일(오늘)이 영업일이면 진행~
 	/**
-	 * 스케쥴러
-	 * 1. 매일 00 시 00 분 05초에 조건에 따라 충전하는 스케쥴러
-	 *     1: 초(0-59)
-	 *     2: 분(0-59)
-	 *     3: 시(0-23)
-	 *     4: 일(1-31)
-	 *     5: 월(1-12)
-	 *     6: 요일(0-7) 0, 7 : 일요일 / 1: 월요일 / 6:토요일
-	* */
+	 * 스케쥴러 1. 매일 00 시 00 분 05초에 조건에 따라 충전하는 스케쥴러 1: 초(0-59) 2: 분(0-59) 3: 시(0-23) 4: 일(1-31) 5:
+	 * 월(1-12) 6: 요일(0-7) 0, 7 : 일요일 / 1: 월요일 / 6:토요일
+	 */
 
-	@Scheduled(cron="${batch.5min.crond}") // 5분마다
-	public void t5minCheck() throws UnsupportedEncodingException, MessagingException {
-		log.info("Path: {}", Util.pathUtils().methodName());
-		log.info("[{}]",Util.dateUtils().now());
+	@Scheduled(cron = "${batch.ones.9AM.crond}") // 아침 9시 한 번
+	public void ones9AM() throws IOException, MessagingException {
+		log.info("[{}]", Util.pathUtils().methodName());
+		batchService.vmDiskUsageCheck();
+	}
+
+	//NOTE: 2023.03.13 twkim 영업일로 별도 처리
+	@Scheduled(cron = "${batch.ones.9AM.crond}") // 아침 9시 한 번(영업일)
+	public void ones9AMWorkTime() throws IOException, MessagingException {
+		log.info("[{}]", Util.pathUtils().methodName());
+		if (isWorkDay()) {    //작업일(오늘)이 영업일이면 진행~
+
+		}
+	}
+
+	@Scheduled(cron = "${batch.1min.crond}") // 1분마다
+	public void every1Min() throws UnsupportedEncodingException, MessagingException {
+		log.info("[{}]", Util.pathUtils().methodName());
 		batchService.processAliveCheck();
 	}
 
-	@Scheduled(cron="${batch.halfHours.crond}") // 30분마다
-	public void halfHoursCheck() throws UnsupportedEncodingException, MessagingException {
-		log.info("Path: {}", Util.pathUtils().methodName());
-		log.info("[{}]",Util.dateUtils().now());
-		if(workDayCheck()){	
+	@Scheduled(cron = "${batch.5min.crond}") // 5분마다
+	public void every5Min() throws UnsupportedEncodingException, MessagingException {
+		log.info("[{}]", Util.pathUtils().methodName());
+
+	}
+
+	@Scheduled(cron = "${batch.30min.crond}") // 30분마다
+	public void every30Min() throws UnsupportedEncodingException, MessagingException {
+		log.info("[{}]", Util.pathUtils().methodName());
+	}
+
+	@Scheduled(cron = "${batch.1hour.workTime.crond}") // 1시간마다
+	public void every1Hour() {
+		log.info("[{}]", Util.pathUtils().methodName());
+	}
+
+	@Scheduled(cron = "${batch.1hour.atMorning.crond}") // 07시 40분 부터 09시 40분까지 1시간 간격(영업일)
+	public void every1HourAtMorning() throws UnsupportedEncodingException, MessagingException {
+		log.info("[{}]", Util.pathUtils().methodName());
+		if (isWorkDay()) {    //작업일(오늘)이 영업일이면 진행~
 
 		}
 	}
 
-	@Scheduled(cron="${batch.workHours.crond}") // 1시간마다(영업일)
-	public void workHoursCheck(){
-		log.info("Path: {}", Util.pathUtils().methodName());
-		log.info("[{}]",Util.dateUtils().now());
-		if(workDayCheck()){	
+	@Scheduled(cron = "${batch.30min.workTime.crond}") // 9시부터 30분마다(영업일)
+	public void every30MinWorkTime() throws UnsupportedEncodingException, MessagingException {
+		log.info("[{}]", Util.pathUtils().methodName());
+		if (isWorkDay()) {    //작업일(오늘)이 영업일이면 진행~
 
 		}
 	}
 
-	@Scheduled(cron="${batch.morning.crond}") // 07시 40분 부터 09시 40분까지 1시간 간격(영업일)
-	public void halfOf7to9Check() throws UnsupportedEncodingException, MessagingException {
-		log.info("Path: {}", Util.pathUtils().methodName());
-		log.info("[{}]",Util.dateUtils().now());
-		if(workDayCheck()){	
-
-		}
+	@Scheduled(cron = "${batch.monthly.crond}") //매월 말일(0시에)
+	public void monthly() throws UnsupportedEncodingException, MessagingException {
+		log.info("[{}]", Util.pathUtils().methodName());
+		batchService.createTable();
 	}
 
-	@Scheduled(cron="${batch.workHalfHours.crond}") // 9시부터 30분마다(영업일)
-	public void workHalfHoursCheck() throws UnsupportedEncodingException, MessagingException {
-		log.info("Path: {}", Util.pathUtils().methodName());
-		log.info("[{}]",Util.dateUtils().now());
-		if(workDayCheck()){	
-
-		}
-	}
-
-	@Scheduled(cron="${batch.morning.oneOff.crond}") // 아침 9시 한 번
-	public void oneOffOnMorning() throws UnsupportedEncodingException, MessagingException {
-		log.info("Path: {}", Util.pathUtils().methodName());
-		log.info("[{}]",Util.dateUtils().now());
-		if(workDayCheck()){	
-
-		}
-	}
-
-	private boolean workDayCheck(){
-		return managerMapper.workDayCheck() == 1 ? true : false;
+	private boolean isWorkDay() {
+		return managerMapper.workDayCheck(Util.dateUtils().yyyyMMdd(0)) == 1 ? true : false;
 	}
 }
