57701ff07d78141ea0aced8df902ff03c4705d29
chris_kim <twkim326@gmail.com>
2023-03-23 10:11:25 +0900

    서버 디스크/메모리 사용량 자동 알림, 월별 테이블 생성 기능 추가
    Scheduled 옵션 명칭 변경, 월말 기능 추가

--- src/main/java/kr/co/ooweat/taskScheduler/service/BatchService.java
+++ src/main/java/kr/co/ooweat/taskScheduler/service/BatchService.java
@@ -5,6 +5,7 @@ import kr.co.ooweat.taskScheduler.common.Util;
 import kr.co.ooweat.taskScheduler.mappers.maria.manager.ManagerMapper;
 import kr.co.ooweat.taskScheduler.model.AlertModel;
 import kr.co.ooweat.taskScheduler.common.ServiceList;
+import kr.co.ooweat.taskScheduler.model.ServiceServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -36,6 +37,19 @@ public class BatchService {
         } else {
             log.info("MariaDB Node Count::{}", galleraNodeCount + " //정상");
         }
+    }
+
+    //DESC: VM 디스크 용량 알림 메소드
+    public void vmDiskUsageCheck() throws IOException, MessagingException {
+        //NOTE: twkim 2023.02.22 리팩토링으로 인한 코드 수정
+        List<HashMap<String, String>> dataList = new ArrayList<>();
+        for (ServiceServer serviceServer : ServiceServer.values()) {
+            dataList.add(Util.connectVM(serviceServer));
+        }
+
+        Alert.smtp(alertModelMaking("system", "ALERT", "vmDiskUsageCheck",
+            "서버 사용량 알림", "서버 사용량 알림", Util.dateUtils().now(),
+            dataList));
     }
 
     //프로세스 생존여부 확인 메소드
@@ -77,5 +91,28 @@ public class BatchService {
         alertModel.setParamListMap(paramListMap);
 
         return alertModel;
+    }
+
+    //NOTE: Spring Scheduler 에 따라 테이블 생성토록.
+    public void createTable() throws MessagingException, UnsupportedEncodingException {
+        int createTableCount = 0;
+        //NOTE: 카카오 Log Table(Void)
+        managerMapper.createKakaoLogYYYYMM(Util.dateUtils().yyyyMM(1));
+        //NOTE: MMS Log Table(Void)
+        managerMapper.createMMSLogYYYYMM(Util.dateUtils().yyyyMM(1));
+
+        createTableCount += managerMapper.checkCreatedKakaoLogTable(Util.dateUtils().yyyyMM(1));
+        createTableCount += managerMapper.checkCreatedMMSLogTable(Util.dateUtils().yyyyMM(1));
+        if (createTableCount > 1) {
+            Alert.smtp(alertModelMaking("system", "ALERT", "createTable",
+                "테이블 생성완료", "월별 테이블 생성 완료(Kakao, MMS)", Util.dateUtils().now()));
+            Alert.kakaoAlert(alertModelMaking("system", "ALERT",
+                "※MariaDB", "월별 테이블" + Util.dateUtils().yyyyMM(1) + " 생성",
+                "월별 테이블 자동 생성 완료 알림(Kakao, MMS)",
+                Util.dateUtils().now()));
+        } else {
+            Alert.kakaoAlert(alertModelMaking("system", "RM", "createTable",
+                "※MariaDB", "월별 테이블 생성 실패 ", Util.dateUtils().now()));
+        }
     }
 }
